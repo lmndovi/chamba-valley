67e126e7b18b3ce078ed84dc43c34f1b7b899504
lmndovi <l.ndovi92@gmail.com>
2023-09-01 22:10:30 +0100

    Image z-index and Header

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <Logo />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Header;
