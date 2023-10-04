d58f09567073548436b3e96f35ba2e2348ddbc62
lmndovi <l.ndovi92@gmail.com>
2023-10-04 15:30:40 +0100

    Home page done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="text-center mt-32 space-y-1 p-5">
+    <div className="text-center mt-20 space-y-1 p-5">
       <div>
         <h2 className="uppercase text-sm font-bold text-[#DC0D0D] ">
           What We Do
@@ -24,7 +24,7 @@ const Hero = () => {
           farming
         </p>
       </div>
-      <div className="flex flex-col mx-auto py-10 h-96 w-full">
+      <div className="flex flex-col mx-auto py-10 h-96 w-full md:w-[600px] md:object-fit">
         <ImageCarousel />
       </div>
       <div>
